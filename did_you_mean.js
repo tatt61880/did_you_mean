d2e90083648a121c4cdfe72192c90969046764c6
tatt61880 <tatt61880@gmail.com>
2022-05-08 05:42:47 +0900

    v1.0.4.1

--- did_you_mean.js
+++ did_you_mean.js
@@ -1,194 +1,262 @@
 // シンプルな(多重配列でない)配列を、cloneメソッドでコピーできるようにする。
 Array.prototype.clone = function() {
-    return Array.apply(null,this)
+	return Array.apply(null, this)
 }
+
 function ArrToStr(arr) {
-    var str = "";
-    for (var i = 0; i < arr.length; i++) {
-        if (0 <= arr[i] && arr[i] <= 9) {
-            str += String(arr[i]);
-        } else if (10 <= arr[i] && arr[i] <= 35) {
-            str += String.fromCharCode(('a').charCodeAt() + arr[i] - 10);
-        } else {
-            alert("Error in ArrToStr(): arr[" + i + "] = " + arr[i]);
-            return "";
-        }
-    }
-    return str;
+	var str = "";
+	for (var i = 0; i < arr.length; i++) {
+		if (!(typeof arr[i] === "number")) {
+			alert("Error in ArrToStr(): arr[" + i + "] isn't a number. " + arr[i]);
+			return "";
+		}
+		if (0 <= arr[i] && arr[i] <= 9) {
+			str += arr[i];
+		} else if (10 <= arr[i] && arr[i] <= 35) {
+			str += String.fromCharCode(('a').charCodeAt() + arr[i] - 10);
+		} else if (36 <= arr[i]) {
+			str += "<z+" + (arr[i] - 35) + ">";
+		} else {
+			alert("Error in ArrToStr(): arr[" + i + "] = " + arr[i]);
+			return "";
+		}
+	}
+	return str;
 }
 
 function StrToArr(str) {
-    var arr = new Array();
-    for (var i = 0; i < str.length; i++) {
-        if ('0' <= str[i] && str[i] <= '9') {
-            arr.push(str[i].charCodeAt() - ('0').charCodeAt());
-        } else if ('a' <= str[i] && str[i] <= 'z') {
-            arr.push(str[i].charCodeAt() - ('a').charCodeAt() + 10);
-        } else {
-            alert("Error in StrToArr(): str[" + i + "] = " + str[i]);
-            return "";
-            return null;
-        }
-    }
-    return arr;
+	var arr = new Array();
+	for (var i = 0; i < str.length; i++) {
+		if ('0' <= str[i] && str[i] <= '9') {
+			arr.push(str[i].charCodeAt() - ('0').charCodeAt());
+		} else if ('a' <= str[i] && str[i] <= 'z') {
+			arr.push(str[i].charCodeAt() - ('a').charCodeAt() + 10);
+		} else {
+			alert("入力可能な文字は、0～9と、a～zです。");
+			//alert("Error in StrToArr(): str[" + i + "] = " + str[i]);
+			return "";
+		}
+	}
+	return arr;
 }
 
 // ジャグリング可能なサイトスワップになっているか否かを判定する。※引数は文字列ではなく、数字の配列。
 function IsJugglable(arr) {
-    if (arr.length == 0) {
-        return false;
-    }
-    var checkArr = new Array(arr.length);
-    for (var i = 0; i < arr.length; i++) {
-        if (arr[i] < 0) { //負の数が入っているものは「ジャグリング不可」とする。()
-            return false;
-        }
-        if (checkArr[(arr[i] + i) % arr.length] != null) {
-            return false;
-        } else {
-            checkArr[(arr[i] + i) % arr.length] = 1;
-        }
-    }
-    return true;
+	if (arr.length == 0) {
+		return false;
+	}
+	var checkArr = new Array(arr.length);
+	for (var i = 0; i < arr.length; i++) {
+		if (arr[i] < 0) { //負の数が入っているものは「ジャグリング不可」とする。()
+			return false;
+		}
+		var index = (arr[i] + i) % arr.length;
+		if (checkArr[index] != null) {
+			return false;
+		} else {
+			checkArr[index] = 1;
+		}
+	}
+	return true;
 }
+
+function ave(arr) {
+	var sum = 0;
+	for (var i = 0, len = arr.length; i < len; ++i) {
+		sum += arr[i];
+	}
+	return sum / arr.length;
+};
+
+var TYPE = {
+	REMOVE:	"1字削除",
+	CHANGE:	"1字変更",
+	INSERT:	"1字挿入",
+	MOVE:	"1字移動",
+	SWAP:	"2字交換",
+};
+
+function ListupDidYouMean(pat) {
+	var ssList = new Array();
+
+	// TODO: Dealing Mulitplex and/or Sync patterns
+	var sum = 0;
+	var average;
+	(function() {
+		for (var i = 0, len = pat.length; i < len; i++) {
+			sum += pat[i];
+		}
+	})();
+	average = ~~(sum / pat.length);
+
+	(function() { // Remove one element
+		// e.g. 551 => 55, 51
+		for(var i = 0; i < pat.length; i++) {
+			var trialPat = pat.clone();
+			trialPat.splice(i, 1);
+			if (IsJugglable(trialPat)) {
+				ssList.push([TYPE.REMOVE, trialPat]);
+			}
+		}
+	})();
+
+	if (sum % pat.length == 0) {
+		(function() { // Swap two elements
+			// e.g. 7351 => 1357, 7531
+			for (var i = 0; i < pat.length; i++) {
+				for (var j = i + 1; j < pat.length; j++) {
+					var trialPat = pat.clone();
+					trialPat[i] = pat[j];
+					trialPat[j] = pat[i];
+					if (IsJugglable(trialPat)) {
+						ssList.push([TYPE.SWAP, trialPat]);
+					}
+				}
+			}
+		})();
+		(function() { // Move one element
+			// e.g. 5371 => 7531
+			for (var i = 0; i < pat.length; i++) { // move element index-i to index-j
+				for (var j = 0; j < pat.length - 1; j++) {
+					if (j < i - 1) {
+						(function() {
+							var trialPat = new Array();
+							Array.prototype.push.apply(trialPat, pat.slice(0, j));
+							trialPat[j] = pat[i];
+							Array.prototype.push.apply(trialPat, pat.slice(j, i));
+							Array.prototype.push.apply(trialPat, pat.slice(i+1, pat.length));
+							if (IsJugglable(trialPat)) {
+								ssList.push([TYPE.MOVE, trialPat]);
+							}
+						})();
+					} else if (j > i + 1) {
+						(function() {
+							var trialPat = new Array();
+							Array.prototype.push.apply(trialPat, pat.slice(0, i));
+							Array.prototype.push.apply(trialPat, pat.slice(i+1, j+1));
+							trialPat[j] = pat[i];
+							Array.prototype.push.apply(trialPat, pat.slice(j+1, pat.length));
+							if (IsJugglable(trialPat)) {
+								ssList.push([TYPE.MOVE, trialPat]);
+							}
+						})();
+					}
+				}
+			}
+		})();
+	} else {
+		(function() { // Change one element
+			var diff1 = (sum % pat.length);              // (large -> small) e.g. 551 => 531
+			var diff2 = pat.length - (sum % pat.length); // (small -> large) e.g. 551 => 561, 552
+			for (var i = 0; i < pat.length; i++) {
+				{
+					var trialPat = pat.clone();
+					trialPat[i] = pat[i] - diff1;
+					if (IsJugglable(trialPat)) {
+						ssList.push([TYPE.CHANGE, trialPat]);
+					}
+				}
+				{
+					var trialPat = pat.clone();
+					trialPat[i] = pat[i] + diff2;
+					if (IsJugglable(trialPat)) {
+						ssList.push([TYPE.CHANGE, trialPat]);
+					}
+				}
+			}
+		})();
+		(function() { // Insert one element (>average)
+			// e.g. 551 => 5551
+			var validSum = (average + 1) * (pat.length + 1);
+			var insertValue = validSum - sum;
+			for (var i = 1; i <= pat.length; i++) {
+				var trialPat = new Array();
+				Array.prototype.push.apply(trialPat, pat.slice(0, i));
+				trialPat[i] = insertValue;
+				Array.prototype.push.apply(trialPat, pat.slice(i, pat.length));
+				if (IsJugglable(trialPat)) {
+					ssList.push([TYPE.INSERT, trialPat]);
+				}
+			}
+		})();
+	}
+
+	(function() { // Insert one element (element <= average)
+		// e.g. 551 => 5151, 5511 (1 < average)
+		// e.g. 135 => 1353 (3 == average)
+		var validSum = average * (pat.length + 1);
+		var insertValue = validSum - sum;
+		for (var i = 1; i <= pat.length; i++) {
+			var trialPat = new Array();
+			Array.prototype.push.apply(trialPat, pat.slice(0, i));
+			trialPat[i] = insertValue;
+			Array.prototype.push.apply(trialPat, pat.slice(i, pat.length));
+			if (IsJugglable(trialPat)) {
+				ssList.push([TYPE.INSERT, trialPat]);
+			}
+		}
+	})();
+
+	return ssList.sort();
+}
+
+var jmjsURL = "http://yuji-k64613.github.io/jmjs/jmjs.html";
 
 // もしかして機能
-function DidYouMean() {
-    var inputStr = Form1.input.value;
-    var pat = StrToArr(inputStr);
-    var ssList = new Array();
+function DidYouMean(ss) {
+	var resultSpan = document.getElementById("resultSpan");
+	var inputStr = Form1.ss.value;
+	if (typeof ss === "undefined") { // ボタンがクリックされたとき。
+		location.href = location.href.split("?")[0] + "?ss=" + Form1.ss.value;
+	} else { // テキストボックスでEnterされたときか、URLに ?ss=○○のパラメータがあるとき。
+		inputStr = ss;
+	}
 
-    if (IsJugglable(pat)) {
-        Form1.output.value = inputStr + " is jugglable.";
-    } else {
-        // TODO: Dealing Mulitplex and/or Sync patterns
-        var sum = 0;
-        var average;
-        (function() {
-            for (var i = 0; i < pat.length; i++) {
-                sum += pat[i];
-            }
-        })();
-        average = ~~(sum / pat.length);
+	if(ss == "") {
+		resultSpan.appendChild( document.createTextNode(inputStr + "何か入力してください。"));
+		return;
+	}
+	var pat = StrToArr(inputStr);
 
-        (function() { // Remove one elemenet
-            // e.g. 551 => 55, 51
-            for(var i = 0; i < pat.length; i++) {
-                var trial_pat = pat.clone();
-                trial_pat.splice(i, 1);
-                if (IsJugglable(trial_pat)) {
-                    ssList.push(ArrToStr(trial_pat));
-                }
-            }
-        })();
+	if (IsJugglable(pat)) {
+		resultSpan.appendChild( document.createTextNode("「"));
+		var link = document.createElement("a");
+		link.href = jmjsURL + "?siteswap=" + inputStr;
+		link.appendChild(document.createTextNode(inputStr));
+		resultSpan.appendChild(link);
+		resultSpan.appendChild( document.createTextNode("」はジャグリング可能な文字列です。←@yuji_k64613さんが作成した、JuggleMaster JavaScript版へのリンクです。") );
+	} else {
+		ssList = ListupDidYouMean(pat);
+	}
 
-        if (sum % pat.length == 0) {
-            (function() { // Swap two element
-                // e.g. 7351 => 1357, 7531
-                for(var i = 0; i < pat.length; i++) {
-                    for(var j = i+1; j < pat.length; j++) {
-                        var trial_pat = pat.clone();
-                        trial_pat[i] = pat[j];
-                        trial_pat[j] = pat[i];
-                        if (IsJugglable(trial_pat)) {
-                            ssList.push(ArrToStr(trial_pat));
-                        }
-                    }
-                }
-            })();
-            (function() { // Moved one elemenet
-                // e.g. 5371 => 7531
-                for (var i = 0; i < pat.length; i++) { // move element index-i to index-j
-                    for (var j = 0; j < pat.length-1; j++) {
-                        if (j < i-1) {
-                            (function() {
-                                var trial_pat = new Array();
-                                Array.prototype.push.apply(trial_pat, pat.slice(0, j));
-                                trial_pat[j] = pat[i];
-                                Array.prototype.push.apply(trial_pat, pat.slice(j, i));
-                                Array.prototype.push.apply(trial_pat, pat.slice(i+1, pat.length));
-                                if (IsJugglable(trial_pat)) {
-                                    ssList.push(ArrToStr(trial_pat));
-                                }
-                            })();
-                        } else if (j > i+1) {
-                            (function() {
-                                var trial_pat = new Array();
-                                Array.prototype.push.apply(trial_pat, pat.slice(0, i));
-                                Array.prototype.push.apply(trial_pat, pat.slice(i+1, j+1));
-                                trial_pat[j] = pat[i];
-                                Array.prototype.push.apply(trial_pat, pat.slice(j+1, pat.length));
-                                if (IsJugglable(trial_pat)) {
-                                    ssList.push(ArrToStr(trial_pat));
-                                }
-                            })();
-                        }
-                    }
-                }
-            })();
-        } else {
-            (function() { // Change one element
-                var diff1 = (sum % pat.length);              // (large -> small) e.g. 551 => 531
-                var diff2 = pat.length - (sum % pat.length); // (small -> large) e.g. 551 => 561, 552
-                for(i = 0; i < pat.length; i++) {
-                    var trial_pat = pat.clone();
-                    trial_pat[i] = pat[i] - diff1;
-                    if (IsJugglable(trial_pat)) {
-                        ssList.push(ArrToStr(trial_pat));
-                    }
-                    trial_pat[i] = pat[i] + diff2;
-                    if (IsJugglable(trial_pat)) {
-                        ssList.push(ArrToStr(trial_pat));
-                    }
-                }
-            })();
-            (function() { // Add one element (>average)
-                // e.g. 551 => 5551
-                var valid_sum = (average+1) * (pat.length + 1);
-                var insert_value = valid_sum - sum;
-                for(i = 1; i <= pat.length; i++) {
-                    var trial_pat = new Array();
-                    Array.prototype.push.apply(trial_pat, pat.slice(0, i));
-                    trial_pat[i] = insert_value;
-                    Array.prototype.push.apply(trial_pat, pat.slice(i, pat.length));
-                    if (IsJugglable(trial_pat)) {
-                        ssList.push(ArrToStr(trial_pat));
-                    }
-                }
-            })();
-        }
-        (function() { // Add one element (element <= average)
-            // e.g. 551 => 5151, 5511 (1 < average)
-            // e.g. 135 => 1353 (3 == average)
-            var valid_sum = average * (pat.length + 1);
-            var insert_value = valid_sum - sum;
-            for(i = 1; i <= pat.length; i++) {
-                var trial_pat = new Array();
-                Array.prototype.push.apply(trial_pat, pat.slice(0, i));
-                trial_pat[i] = insert_value;
-                Array.prototype.push.apply(trial_pat, pat.slice(i, pat.length));
-                if (IsJugglable(trial_pat)) {
-                    ssList.push(ArrToStr(trial_pat));
-                }
-            }
-        })();
-
-        //alert(ArrToStr(pat));
-
-        // output
-        if (ssList.length == 0) {
-            Form1.output.value = inputStr + " is not jugglable.";
-        } else {
-            Form1.output.value = "Did you mean:\n";
-            ssList = ssList.sort();
-            var last = "";
-            ssList.forEach(function(siteswap){
-                if (last != siteswap) {
-                    last = siteswap;
-                    Form1.output.value += " " + siteswap + "\n";
-                }
-            })
-        }
-    }
+	// output
+	if (ssList.length == 0) {
+		resultSpan.appendChild(document.createTextNode("「" + inputStr + "」はジャグリング不可能な文字列です。\n(似た文字列にもジャグリング可能な物はありません。)"));
+	} else {
+		resultSpan.appendChild(document.createTextNode("入力文字列: " + inputStr + "\nもしかして: (下表をご参照ください) \n"));
+		var table = document.getElementById("resultTable");
+		var row = table.insertRow();
+		var thObj = document.createElement("th"); thObj.appendChild( document.createTextNode("Siteswap")); row.appendChild( thObj );
+		var thObj = document.createElement("th"); thObj.appendChild( document.createTextNode("ボールの数")); row.appendChild( thObj );
+		var thObj = document.createElement("th"); thObj.appendChild( document.createTextNode("入力文字列との関係")); row.appendChild( thObj );
+		var thObj = document.createElement("th"); thObj.appendChild( document.createTextNode("@yuji_k64613さんが作成した、JuggleMaster JavaScript版へのリンク。")); row.appendChild( thObj );
+		ssList = ssList.sort();
+		var last = "";
+		ssList.forEach(function(siteswapArray) {
+			var method = siteswapArray[0];
+			var siteswap = ArrToStr(siteswapArray[1]);
+			var num = ave(siteswapArray[1]);
+			if (last != siteswap && !siteswap.match(/</)) { // 「<z+α>」が含まれると、JavaScript版JuggleMasterへのリンクが正常にならないため、省きます。
+				last = siteswap;
+				var row = table.insertRow();
+				row.insertCell().appendChild( document.createTextNode( siteswap ) );
+				row.insertCell().appendChild( document.createTextNode( num ) );
+				row.insertCell().appendChild( document.createTextNode( method ) );
+				var link = document.createElement("a");
+				link.href = jmjsURL + "?siteswap=" + siteswap;
+				link.appendChild(document.createTextNode(link.href));
+				row.insertCell().appendChild(link);
+			}
+		})
+	}
 }
-
---
+++ test.js
@@ -0,0 +1,129 @@
+var passedCount = 0;
+var failedCount = 0;
+var passedTable;
+var failedTable;
+
+Array.prototype.equals = function(arr) {
+	if (arr instanceof Array) {
+		if (this.length != arr.length) {
+			return false;
+		}
+		var f = true;
+		for (var i = 0; i < this.length; ++i) {
+			if (this[i] instanceof Array) {
+				f = this[i].equals(arr[i]);
+			} else {
+				f = this[i] == arr[i];
+			}
+			if (f == false) break;
+		}
+		return f;
+	} else {
+		return false;
+	}
+}
+
+Array.prototype.ToStr = function() {
+	var str = "[";
+	for (var i = 0; i < this.length; ++i) {
+		if (this[i] instanceof Array) {
+			str += this[i].ToStr();
+		} else {
+			if (typeof(this[i]) == 'string') {
+				str += '"' + this[i] + '"';
+			} else {
+				str += this[i];
+			}
+		}
+		if(i != this.length - 1) str += ",";
+	}
+	str += "]";
+	return str;
+}
+
+
+function PrintResult(functionName, parameters, actual, expected) {
+	if (actual === expected) {
+		passedCount++;
+		var row = passedTable.insertRow();
+		row.insertCell().appendChild( document.createTextNode( functionName ) );
+		row.insertCell().appendChild( document.createTextNode( parameters ) );
+		row.insertCell().appendChild( document.createTextNode( actual ) );
+	} else {
+		failedCount++;
+		var row = failedTable.insertRow();
+		row.insertCell().appendChild( document.createTextNode( functionName ) );
+		row.insertCell().appendChild( document.createTextNode( parameters ) );
+		row.insertCell().appendChild( document.createTextNode( actual ) );
+		row.insertCell().appendChild( document.createTextNode( expected ) );
+	}
+}
+
+function assertIsJugglable(SS, exp) {
+	var actual = IsJugglable(StrToArr(SS));
+	PrintResult("IsJugglable", 'StrToArr("' + SS + '")', actual, exp);
+}
+
+function assertDidYouMean(SS, exp) {
+	//console.log('.');
+	//console.assert(actual.equals(expected), '\nact: ' + actual.ToStr() + '\nexp: ' + expected.ToStr());
+
+	var actual		= ListupDidYouMean(StrToArr(SS)).sort().ToStr();
+	var expected	= exp.sort().ToStr();
+	PrintResult("ListupDidYouMean", 'StrToArr("' + SS + '")', actual, expected);
+}
+
+function TestIsSiteswap() {
+	assertIsJugglable("0", true);
+	assertIsJugglable("1", true);
+	assertIsJugglable("2", true);
+	assertIsJugglable("a", true);
+	assertIsJugglable("10", false);
+	assertIsJugglable("11", true);
+	assertIsJugglable("531", true);
+	assertIsJugglable("551", false);
+	assertIsJugglable("7441", true);
+	assertIsJugglable("db97531", true);
+	assertIsJugglable("31416", true);
+	assertIsJugglable("3141592", false);
+	assertIsJugglable("siteswap", false);
+	assertIsJugglable("siteawsp", true);
+	assertIsJugglable("sitaeswp", true);
+}
+
+function TestDidYouMean() {
+	assertDidYouMean("7351", [[TYPE.SWAP,[1,3,5,7]], [TYPE.SWAP,[7,5,3,1]], [TYPE.MOVE,[3,5,7,1]], [TYPE.MOVE,[7,1,3,5]], [TYPE.INSERT,[7,3,4,5,1]]]);
+	assertDidYouMean("siteswap", [[TYPE.MOVE,[28,18,29,10,14,28,32,25]],[TYPE.SWAP,[28,18,29,14,10,32,28,25]]]);
+	assertDidYouMean("551", [["1字削除",[5,1]],["1字削除",[5,5]],["1字変更",[5,3,1]],["1字変更",[5,5,2]],["1字変更",[5,6,1]],["1字追加",[5,1,5,1]],["1字追加",[5,5,1,1]],["1字追加",[5,5,1,5]],["1字追加",[5,5,5,1]]]);
+}
+
+function TestAll() {
+	passedTable = document.getElementById("PassedResultTable");
+	var row = passedTable.insertRow();
+	var thObj = document.createElement("th"); thObj.appendChild( document.createTextNode("Function"));	row.appendChild( thObj );
+	var thObj = document.createElement("th"); thObj.appendChild( document.createTextNode("Parameter"));	row.appendChild( thObj );
+	var thObj = document.createElement("th"); thObj.appendChild( document.createTextNode("Return"));	row.appendChild( thObj );
+
+	failedTable = document.getElementById("FailedResultTable");
+	var row = failedTable.insertRow();
+	var thObj = document.createElement("th"); thObj.appendChild( document.createTextNode("Function"));	row.appendChild( thObj );
+	var thObj = document.createElement("th"); thObj.appendChild( document.createTextNode("Parameter"));	row.appendChild( thObj );
+	var thObj = document.createElement("th"); thObj.appendChild( document.createTextNode("Return"));	row.appendChild( thObj );
+	var thObj = document.createElement("th"); thObj.appendChild( document.createTextNode("Expected"));	row.appendChild( thObj );
+
+	TestIsSiteswap();
+	TestDidYouMean();
+
+	if (failedCount == 0) {
+		var notesSpan = document.getElementById("Notes");
+		notesSpan.appendChild( document.createTextNode("Passed all " + passedCount + " tests!") );
+		failedTable.deleteRow(0);
+	} else {
+		var passedSpan = document.getElementById("PassedResult");
+		passedSpan.appendChild( document.createTextNode("Passed " + passedCount + " tests.") );
+		var failedSpan = document.getElementById("FailedResult");
+		failedSpan.appendChild( document.createTextNode("Failed " + failedCount + " tests.") );
+		var notesSpan = document.getElementById("Notes");
+		notesSpan.appendChild( document.createTextNode("There are failed tests below.") );
+	}
+}
